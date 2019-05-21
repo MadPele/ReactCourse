b6a3d50cf529b25c4b0653e562796bd87144ff26
Andrzej Derela <andrzejderela@gmail.com>
2019-05-21 20:03:23 +0200

    v.159

---
+++ React/blog/src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import PostList from './PostList'
+
+export class App extends React.Component{
+
+    render(){
+        return (<div className='ui container'><PostList /></div>)
+    }
+}
