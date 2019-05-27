2ba8c35a898f1f879914bd658ea814dbfb03c2fe
Andrzej Derela <andrzejderela@gmail.com>
2019-05-27 20:26:26 +0200

    v.206

--- React/streams/client/src/components/App.js
+++ React/streams/client/src/components/App.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { BrowserRouter, Router } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamCreate from "./streams/Stream.Create";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
-class App extends React.Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+const App = () => {
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
