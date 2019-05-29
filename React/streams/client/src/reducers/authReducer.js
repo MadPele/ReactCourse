ad3496eedb9c460809eb1c448ce914c4d0337c19
Andrzej Derela <andrzejderela@gmail.com>
2019-05-29 20:01:07 +0200

    v.218

---
+++ React/streams/client/src/reducers/authReducer.js
@@ -0,0 +1,14 @@
+const INITIAL_STATE = {
+  isSignedIn: null
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "SIGN_IN":
+      return { ...action, isSignedIn: true };
+    case "SIGN_OUT":
+      return { ...action, isSignedIn: false };
+    default:
+      return state;
+  }
+};
