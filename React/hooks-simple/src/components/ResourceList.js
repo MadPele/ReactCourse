3ac5dc32bf3a4a327da89a279b8d9f2e88da145f
Andrzej Derela <andrzejderela@gmail.com>
2019-06-17 20:01:48 +0200

    v.336

---
+++ React/hooks-simple/src/components/ResourceList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import useResources from "./useResource";
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
+  return (
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title ? record.title : record.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default ResourceList;
--- React/seasons/src/index.js
+++ React/seasons/src/index.js
@@ -2,32 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 import Spiner from "./Spiner";
+import useLocation from "./useLocation";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: null,
-      errorMessage: ""
-    };
+const App = () => {
+  const [lat, error] = useLocation();
+
+  let content;
+  if (error) {
+    content = <div>Error: {error}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spiner message="Please accept location request" />;
   }
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
-    );
-  }
-
-  render() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-    return <Spiner message="Please accept location request" />;
-  }
-}
+  return <div className="border red">{content}</div>;
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
